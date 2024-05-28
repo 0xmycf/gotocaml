4d377dc02a6e6a8d12ed414b992209aa60220fc4
0xmycf <mycf.mycf.mycf@gmail.com>
2024-05-29 01:02:54 +0200

    readme example

--- bin/main.ml
+++ bin/main.ml
@@ -23,22 +23,49 @@ let run blocks =
        | Invalid_argument _ -> print_endline "oopsie"
   in aux 0
 
-let () =
-  print_newline();
-  let i = ref 0 in
+(* let () = *)
+(*   print_newline(); *)
+(*   let i = ref 0 in *)
+(*   run [| *)
+(*     0>> begin fun goto -> *)
+(*       if !i < 10 then () else goto 1; *)
+(*       Printf.printf "%d: Hello, OCaml!\n" !i; *)
+(*       i := !i + 1; *)
+(*       goto 0; *)
+(*     end; *)
+(*     1>> begin fun _goto -> *)
+(*       print_endline "Goodbye, OCaml!"; *)
+(*     end; *)
+(*     2>> begin fun goto -> *)
+(*       i := 0; *)
+(*       print_endline "Lets start anew"; *)
+(*       goto 0 *)
+(*     end *)
+(*       |] *)
+
+
+let () = 
+  let i = ref 10_000 in
   run [|
-    0>> begin fun goto ->
-      if !i < 10 then () else goto 1;
-      Printf.printf "%d: Hello, OCaml!\n" !i;
-      i := !i + 1;
-      goto 0;
-    end;
-    1>> begin fun _goto ->
-      print_endline "Goodbye, OCaml!";
-    end;
-    2>> begin fun goto ->
-      i := 0;
-      print_endline "Lets start anew";
-      goto 0
-    end
-      |]
+  0 >> begin fun goto -> 
+    print_endline "Hello world!";
+    goto 2
+  end;
+  1 >> begin fun goto -> 
+    print_endline "Exit";
+    goto 4
+  end;
+  2 >> begin fun  goto ->
+    i := !i - 1;
+    if !i mod 2 = 0 then goto 3 else goto 2
+  end;
+  3 >> begin fun goto -> 
+    i := (!i / 2) + 1;
+    Printf.printf "%d\n" !i;
+    if !i <= 1 then goto 1 else goto 2; 
+  end;
+  4 >> begin fun _goto -> 
+    print_endline "Goodbye world!";
+    exit 0
+  end
+  |]
--- readme.md
+++ readme.md
@@ -1,2 +1,32 @@
 This is probably not the best way to do it and CPS is not really used either
 but it works. Labels are also just numeric, which sucks too.
+
+You can write stuff like:
+
+```ocaml
+let () = 
+  let i = ref 10_000 in
+  run [|
+  0 >> begin fun goto -> 
+    print_endline "Hello world!";
+    goto 2
+  end;
+  1 >> begin fun goto -> 
+    print_endline "Exit";
+    goto 4
+  end;
+  2 >> begin fun goto ->
+    i := !i - 1;
+    if !i mod 2 = 0 then goto 3 else goto 2
+  end;
+  3 >> begin fun goto -> 
+    i := (!i / 2) + 1;
+    Printf.printf "%d\n" !i;
+    if !i <= 1 then goto 1 else goto 2; 
+  end;
+  4 >> begin fun _goto -> 
+    print_endline "Goodbye world!";
+    exit 0
+  end
+  |]
+```
